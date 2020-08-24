8f26b328b69cad6ab818f96065c9516ea2cca440
xy <xy18043124001@gmail.com>
2020-08-24 11:20:12 +0800

    string 2 pdf

--- demoutil/src/main/java/com/example/demo/pdf/htmlToPdf.java
+++ demoutil/src/main/java/com/example/demo/pdf/htmlToPdf.java
@@ -1,11 +1,13 @@
 package com.example.demo.pdf;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 import lombok.Cleanup;
-import org.apache.commons.codec.binary.Base64;
 
 /**
  * html转pdf
@@ -18,15 +20,22 @@ public class htmlToPdf {
     private static final String LOGO_PATH = "/Users/fuzi/workspace/doc/pdf/111.png";
 
     public static void main(String[]args) throws Exception {
-        create();
+        String html ="<!DOCTYPE html> <html> <head> <title>Title</title> <style> body{ font-family:SimHei; } .color{ color: green; } .pos{ position:absolute; left:200px; top:5px; width: 200px; font-size: 10px; } @media print { div.header-right { display: block; position: running(header-right); } } @page { size: 8.5in 11in; @top-right { content: element(header-right) }; @bottom-center { content: element(footer) } } #footer { position: running(footer); } #pages:before { content: counter(page); } #pages:after { content: counter(pages); } </style> </head> <body> <div id=\"footer\"> <div style=\"text-align: center; width: 100%;font-size: 15px;\">Page <span id=\"pages\"> of </span></div> </div> <div class=\"page\"> <div class=\"color\">你好，${name1}</div> </div> <div class=\"page\"> <div class=\"color\">你好，${name2}</div> </div> <div class=\"page\"> <div class=\"color\">你好，${name}</div> <img src=\"${file64Str}\" width=\"100px\" /> </div> </body> </html>";
+
+        Map<String,String> map = new HashMap<>();
+        map.put("${file64Str}",LOGO_PATH);
+        map.put("${name}","xxxxxxx");
+        map.put("${name1}","sssssss");
+        map.put("${name2}","yyyyyy");
+        create(map,html);
     }
 
-    public static void create() throws Exception {
-        String html ="<!DOCTYPE html> <html> <head> <meta charset=\"UTF-8\"/> <title>Title</title> <style> body{ font-family:SimHei; } .color{ color: green; } .pos{ position:absolute; left:200px; top:5px; width: 200px; font-size: 10px; } @media print { div.header-right { display: block; position: running(header-right); } } @page { size: 8.5in 11in; @top-right { content: element(header-right) }; /*@bottom-center { content : \"Page \" counter(page) \" of \" counter(pages); };\t */ @bottom-center { content: element(footer) } } #footer { position: running(footer); } #pages:before { content: counter(page); } #pages:after { content: counter(pages); } </style> </head> <body style='font-size:20px;font-family:SimSun;'> <div id=\"footer\"> <div style=\"text-align: center; width: 100%;font-size: 15px;\">Page <span id=\"pages\"> of </span></div> </div> <div class=\"page\"> <div class=\"color\">你好，${name}222</div> <img src=\"${file64Str}\" width=\"600px\" /> </div> </body> </html>";
-        html =  html.replace("${name}","xxxxxxx").replace("${file64Str}",LOGO_PATH);
-
+    public static void create(Map<String,String> map, String html) throws Exception {
+        for (String key : map.keySet()) {
+            html = html.replace(key,map.get(key));
+        }
         @Cleanup  InputStream is = new ByteArrayInputStream(html.getBytes(StandardCharsets.UTF_8));
-        @Cleanup  OutputStream os = new FileOutputStream("/Users/fuzi/workspace/demo.4.pdf");
+        @Cleanup  OutputStream os = new FileOutputStream("/Users/fuzi/workspace/demo.pdf");
         Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document,os);
         document.open();
