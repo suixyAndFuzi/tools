a2f4dcc20833e4fd5bb27a08ef471527db34e752
xy <xy18043124001@gmail.com>
2021-05-08 17:05:31 +0800

    fix---pdf模版生成pdf

--- demoutil/src/main/java/com/example/demo/pdf/WordToPdf.java
+++ demoutil/src/main/java/com/example/demo/pdf/WordToPdf.java
@@ -1,14 +1,18 @@
 package com.example.demo.pdf;
 
-import  com.itextpdf.text.*;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.*;
 import lombok.Cleanup;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -27,66 +31,78 @@ class WordToPdfBean{
 
 /**
  * word 方式生成pdf模版
+ *
  * @author fuzi
  */
 public class WordToPdf {
-    /**生成的新文件路径**/
-    private static final String NEW_PDF_PATH = "/Users/fuzi/workspace/doc/pdf/recipeNew.pdf";
-    /**模板路径*/
-    private static final String TEMPLATE_PATH = "/Users/fuzi/workspace/doc/pdf/recipe.pdf";
+    /**
+     * 生成的新文件路径
+     **/
+    //private static final String NEW_PDF_PATH = "/Users/fuzi/workspace/doc/pdf/recipeDrug.pdf";
+    private static final String DEST = "/Users/fuzi/Downloads/add.pdf";
+    /**
+     * 模板路径
+     */
+    private static final String TEMPLATE_PATH = "/Users/fuzi/github/tools/doc/pdf/recipeDrug.pdf";
 
-    private static final String IMG = "/Users/fuzi/workspace/doc/pdf/111.png";
+    private static final String IMG = "/Users/fuzi/github/tools/doc/pdf/111.png";
 
     public static void main(String[] args) {
-        WordToPdfBean w = new  WordToPdfBean("organInfo","福建省立医院互联网医院",1);
-        WordToPdfBean w2 = new  WordToPdfBean("patientId","3831291",1);
-        WordToPdfBean w3 = new  WordToPdfBean("recipeCode","ngri121345689",1);
-        WordToPdfBean w4 = new  WordToPdfBean("pName","昱平",1);
-        WordToPdfBean w5 = new  WordToPdfBean("pGender","男",1);
-        WordToPdfBean w6 = new  WordToPdfBean("pAge","123",1);
 
-        WordToPdfBean w7 = new  WordToPdfBean("pType","自费",1);
-        WordToPdfBean w8 = new  WordToPdfBean("departInfo","时间科",1);
-        WordToPdfBean w9 = new  WordToPdfBean("cDate","2008-12-08 13:62",1);
-        WordToPdfBean w10 = new  WordToPdfBean("mobile","13697238800",1);
-        WordToPdfBean w11 = new  WordToPdfBean("disease","的环境的看哈客户端画口红的哈哈哈看见好看好看花见花开卡号很快就和空间水水水水还是",1);
-        WordToPdfBean w77 = new  WordToPdfBean("mytitle","222222222",1);
+        WordToPdfBean w = new WordToPdfBean("organInfo", "福建省立医院互联网医院", 1);
+        WordToPdfBean w2 = new WordToPdfBean("patientId", "3831291", 1);
+        WordToPdfBean w3 = new WordToPdfBean("recipeCode", "ngri12", 1);
+        WordToPdfBean w16 = new WordToPdfBean("recipeCode1", "ngri121345689", 1);
+        WordToPdfBean w4 = new WordToPdfBean("pName", "昱平", 1);
+        WordToPdfBean w5 = new WordToPdfBean("pGender", "男", 1);
+        WordToPdfBean w6 = new WordToPdfBean("pAge", "123", 1);
+
+        WordToPdfBean w7 = new WordToPdfBean("pType", "自费", 1);
+        WordToPdfBean w8 = new WordToPdfBean("departInfo", "时间科", 1);
+        WordToPdfBean w9 = new WordToPdfBean("cDate", "2008-12-08 13:62", 1);
+        WordToPdfBean w10 = new WordToPdfBean("mobile", "13697238800", 1);
+        WordToPdfBean w11 = new WordToPdfBean("disease", "的环境的看哈客户端画口红的哈哈哈看见好看好看花见花开卡号很快就和空间水水水水还是", 1);
+        WordToPdfBean w12 = new WordToPdfBean("myTitle", "222222222", 1);
 //        WordToPdfBean w3 = new  WordToPdfBean("xxx",IMG,2);
-//        WordToPdfBean w4 = new  WordToPdfBean("yyy",IMG,2);
-        List<WordToPdfBean> list = Arrays.asList(w,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w77);
+        WordToPdfBean w13 = new WordToPdfBean("yyy", IMG, 2);
+
+        WordToPdfBean w14 = new WordToPdfBean("recipeType", "普通", 1);
+        WordToPdfBean w15 = new WordToPdfBean("weight", "45", 1);
+        List<WordToPdfBean> list = Arrays.asList(w, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16);
         try {
             pdfOut(list);
         } catch (IOException | DocumentException e) {
             System.out.println(e);
         }
     }
-    public static final String DEST = "/Users/fuzi/Downloads/add.pdf";
-    public static final String DEST1 = "/Users/fuzi/Downloads/add1.pdf";
+
+
     /**
      * pdf 读取输出流 方法
+     *
      * @param list
      */
-    public static void pdfOut(List<WordToPdfBean> list) throws IOException, DocumentException{
-            // 读取pdf模板
-            PdfReader reader = new PdfReader(DEST);
-            @Cleanup  ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            PdfStamper stamper = new PdfStamper(reader, bos);
-            AcroFields form = stamper.getAcroFields();
-            form.addSubstitutionFont(BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED));
-            //组织参数
-            pdf(list,form);
-            // 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
-            stamper.setFormFlattening(false);
-            stamper.close();
+    public static void pdfOut(List<WordToPdfBean> list) throws IOException, DocumentException {
+        // 读取pdf模板
+        PdfReader reader = new PdfReader(TEMPLATE_PATH);
+        @Cleanup ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PdfStamper stamper = new PdfStamper(reader, bos);
+        AcroFields form = stamper.getAcroFields();
+        form.addSubstitutionFont(BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED));
+        //组织参数
+        pdf(list, form);
+        // 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
+        stamper.setFormFlattening(false);
+        stamper.close();
 
-            // 输出流
-            @Cleanup FileOutputStream out = new FileOutputStream(DEST1);
-            Document doc = new Document();
-            PdfSmartCopy copy = new PdfSmartCopy(doc, out);
-            doc.open();
-            PdfImportedPage importPage = copy.getImportedPage(new PdfReader(bos.toByteArray()), 1);
-            copy.addPage(importPage);
-            doc.close();
+        // 输出流
+        @Cleanup FileOutputStream out = new FileOutputStream(DEST);
+        Document doc = new Document();
+        PdfSmartCopy copy = new PdfSmartCopy(doc, out);
+        doc.open();
+        PdfImportedPage importPage = copy.getImportedPage(new PdfReader(bos.toByteArray()), 1);
+        copy.addPage(importPage);
+        doc.close();
     }
 
     /**
@@ -96,10 +112,10 @@ public class WordToPdf {
      */
     private static void pdf(List<WordToPdfBean> list, AcroFields form) throws IOException, DocumentException{
         for (WordToPdfBean a:list) {
-            if(1==a.getType()){
+            if (1 == a.getType()) {
                 //文字类的内容处理
-                form.setField(a.getKey(),a.getValue());
-            }else {
+                form.setField(a.getKey(), a.getValue());
+            } else {
                 //将图片写入指定的field
                 String key = a.getKey();
                 Image image = Image.getInstance(a.getValue());
@@ -108,5 +124,16 @@ public class WordToPdf {
                 form.replacePushbuttonField(key, pb.getField());
             }
         }
+
+        getPdfTextPosition(form);
+    }
+
+
+    public static void getPdfTextPosition(AcroFields fields) {
+        List<AcroFields.FieldPosition> pos = fields.getFieldPositions("recipeType");
+        AcroFields.FieldPosition pitem = pos.get(0);
+        Rectangle pRectangle = pitem.position;
+        String res = pRectangle.getLeft() + "," + pRectangle.getBottom();
+        System.out.println(res);
     }
 }
